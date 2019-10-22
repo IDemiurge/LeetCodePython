32b8141060f6d627f4c7c71f5276d071da50666b
EiDemiurge <justmeakk@gmail.com>
2019-10-22 19:33:41 +0700

    added romanNumbers problem

---
+++ algs/roman.py
@@ -0,0 +1,24 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        switchMap = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+        result = 0
+        maxVal = 0
+        for char in reversed(list(s)):
+            val = switchMap.get(char)
+            if maxVal <= val:
+                maxVal = val
+                result += val
+            else:
+                result -= val
+
+        return result
+
+    print(romanToInt(None, "LVIII"))
